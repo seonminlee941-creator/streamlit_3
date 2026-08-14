430ea4bc6d6552ac5ac699a1a6f071dbcb2b9652
Selena <seonminlee941@gmail.com>
2026-08-14 11:48:39 -0700

    API

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if not API_KEY:
     API_KEY = os.getenv("GROQ_API_KEY")
 
 if not API_KEY:
-    st.error("GROQ_API_KEY is not yet found.")
+    st.error("GROQ_API_KEY is not yet found")
 
 MODEL = "llama-3.1-8b-instant"
 client = Groq(api_key=API_KEY)
